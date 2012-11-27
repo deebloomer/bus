04a3a586e167f3d2af4ed0a3331e3f3837ce0769
Deidre Bloomer-Daly <deebloomer@hotmail.com>
2012-11-27 16:37:59 +0000

    DB added info to seeds and changed models  product.rb : length of title to require 5 characters to Pauls file

---
+++ db/seeds.rb
@@ -0,0 +1,52 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.destroy_all
+
+Product.create({title: 'WexDubZ1',
+               description: %{<p> 'Wexford (Redmond Sq), Oylgate, Enniscorthy, Ferns',</p>},
+               image_url: 'cs.jpg',
+               price: 6.00})
+
+Product.create({title: 'WexDubZ2',
+               description: %{<p>'Camolin, Gorey, Arklow (Old Dublin Rd)*** ',    </p>},
+               image_url: 'ruby.jpg',
+               price: 6.00})
+
+Product.create({title: 'WexDubZ3',
+                description: "Cherrywood / Wyattville Rd (LUAS Grn Line)*, Montrose Hotel (UCD), Lr Merrion St (Davenport Hotel), Georges Quay, O2 Arena (Clarion Hotel), Dublin Airport (Coach Pk Zone 14), ",
+                image_url: 'ruby.jpg',
+                price: 6.00})
+
+
+Product.create({title: 'WexDubZ1_3',
+               description: %{<p>'Wexford to Dublin',</p>},
+               image_url: 'rtp.jpg',
+               price: 15.00})
+
+Product.create({title: 'DubWexZ1',
+                description: 'Dublin Airport (Coach Pk Zone 14), O2 Arena (Clarion Hotel), Georges Quay, Lr Merrion St (Davenport Hotel), Montrose Hotel (UCD), Cherrywood / Wyattville Rd (LUAS Grn Line)*',
+                image_url: 'ruby.jpg',
+                price: '6.00'})
+
+
+Product.create({title: 'DubWexZ2',
+               description: 'Arklow (Old Dublin Rd)***, Gorey, Camolin',
+               image_url: 'ruby.jpg',
+               price: '6.00'})
+
+
+Product.create({title: 'DubWexZ3',
+               description: 'Ferns, Enniscorthy, Oylgate, Wexford (Redmond Sq)',
+               price: '6.00'})
+
+Product.create({title: 'DubWexZ1_3',
+               description: 'Dublin to Wexford',
+               image_url: 'ruby.jpg',
+               price:'15.00'})
+
